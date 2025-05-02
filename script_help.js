fa62795807ea7e91fb9d7c997939e84d4f70480b
4245877 <alner4245877@gmail.com>
2025-05-02 14:18:51 +0300

    Описание изменений

--- script_help.js
+++ script_help.js
@@ -204,3 +204,12 @@ function checkRequirements() {
 
     document.getElementById('result').innerHTML = result;
 }
+
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
