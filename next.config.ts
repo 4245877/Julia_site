a804a68c209cb3e6bda14249a398028f8a2862c9
4245877 <alner4245877@gmail.com>
2026-05-01 18:30:06 +0300

    123

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS === "true";
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+
+  output: "export",
+  trailingSlash: true,
+
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: isGithubActions ? "/Julia_site" : "",
+  assetPrefix: isGithubActions ? "/Julia_site/" : "",
 };
 
 export default nextConfig;
